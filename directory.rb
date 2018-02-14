ebbe42f23e24411dd10875b24a896f50a73401c1
Cindy <jl7e12@gmail.com>
2018-02-14 00:12:52 +0000

    ex4 - replace each method with while loop in Print method

--- directory.rb
+++ directory.rb
@@ -21,8 +21,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  index = 0
+  while index < students.length
+    puts "#{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    index += 1
   end
 end
 
